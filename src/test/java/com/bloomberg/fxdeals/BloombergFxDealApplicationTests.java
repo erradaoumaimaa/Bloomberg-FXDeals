42fd20b42779bdecca5d7774f1914185737de3b9
ER-RADA Oumaima <erradaoumaima@gmail.com>
2025-02-20 23:17:02 +0100

    first commit

---
+++ src/test/java/com/bloomberg/fxdeals/BloombergFxDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomberg.fxdeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloombergFxDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
